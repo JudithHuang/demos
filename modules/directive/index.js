0165abdb24eb4bbd76d7345056551bf302a9252e
Judithhuang <Judithhuang@augmentum.com.cn>
2015-09-25 16:36:52 +0800

    [angular-directive] test for .scroll

--- modules/directive/index.js
+++ modules/directive/index.js
@@ -7,28 +7,38 @@ angular.module('app', [])
   return {
     restrict: 'E',
     templateUrl: 'teplate.html',
+    scope: {
+      judithStr: '@?'
+    },
     link: function(scope, elem, attr) {
-      console.log('this is method link');
-    },
-    compile: function(elem, attr) {
-      return {
-        pre: function(scope, elem, attr, controller, transcludeFn) {
-          console.log('==this is method compile-pre==');
-          console.log(scope);
-          var html = elem.children().clone();
-          elem.append(html);
-          console.log(elem.children());
-        },
-        post: function() {
-          console.log('this is method compile-post');
-        }
+      console.log('=======================this is method link');
+      if (scope.judithStr) {
+        console.log(scope.judithStr);
       }
-    },
-    controller: function($scope) {
-      $scope.username = 'judith';
-      console.log('==this is method controller==');
-      console.log($scope);
-    },
-    controllerAs: 'ctrl'
+
+      $(elem).find('.container').scroll(function() {
+        console.log('==scrolling==');
+      });
+    }
+  //   compile: function(elem, attr) {
+  //     return {
+  //       pre: function(scope, elem, attr, controller, transcludeFn) {
+  //         console.log('==this is method compile-pre==');
+  //         console.log(scope);
+  //         var html = elem.children().clone();
+  //         elem.append(html);
+  //         console.log(elem.children());
+  //       },
+  //       post: function() {
+  //         console.log('this is method compile-post');
+  //       }
+  //     }
+  //   },
+  //   controller: function($scope) {
+  //     $scope.username = 'judith';
+  //     console.log('==this is method controller==');
+  //     console.log($scope);
+  //   },
+  //   controllerAs: 'ctrl'
   }
 });
