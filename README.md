66bef556e61269d3776c982915dfcb854f6990c4
Judithhuang <judithhuang@augmentum.com.cn>
2016-10-10 15:44:59 +0800

    book: css3

---
+++ README.md
@@ -0,0 +1,7 @@
+# Directory
+
+## 深入理解 JavaScript
+  ./speakingJavaScript
+
+## HTML 5 与 CSS3 权威指南(下册)
+  ./css3
---
+++ css3/index.php
@@ -0,0 +1,13 @@
+<?php
+
+    echo 'hello';
+
+    $obj = [
+        'a' => 'dddd',
+        'b' => 2,
+        'c' => 3
+    ];
+
+    list($a) = $obj;
+
+    echo $a;
