37804b51ff8d59e1f1171bd5bf13b317c5431323
Judithhuang <judithhuang@augmentum.com.cn>
2016-10-13 08:09:01 +0800

    speakingJavaScript: chapter20

---
+++ speakingJavaScript/chapter20/index.js
@@ -0,0 +1,40 @@
+// 20.1
+// eg.1
+// console.log(new Date('2004-08-29'));
+// console.log(new Date(0));
+
+
+// 20.2
+// eg.1
+// console.log(Date.parse('2004-08-29'));
+// console.log(+new Date('2004-08-29'));
+// console.log(Date.UTC(2004, 7, 29));
+
+
+// 20.3
+// eg.1
+// var date = new Date();
+// console.log('day:', date.getUTCDate());
+// console.log('month:', date.getUTCMonth());
+// console.log('TimezoneOffset:', date.getTimezoneOffset());
+
+
+// 20.3.3
+// eg.1
+// var date = new Date();
+// console.log(date.toTimeString());
+// console.log(date.toLocaleTimeString());
+
+// console.log(date.toDateString());
+// console.log(date.toLocaleDateString());
+
+// console.log(date.toString());
+// console.log(date.toLocaleString());
+// console.log(date.toUTCString());
+// console.log(date.toJSON());
+
+
+// 20.4
+// 20.4.2
+// eg.1
+console.log(new Date('T13:17'));
---
+++ speakingJavaScript/chapter21/index.js
@@ -0,0 +1,16 @@
+// var arr = [{ x: '1' }, { x: '3' }, { x: '2' }];
+// arr.sort(function(pre, next) {
+//   return next.x - pre.x;
+// }, function() {
+//   return true;
+// });
+
+// console.log(arr);
+
+// var result = [];
+
+// arr.forEach(function(elem) {
+//   result.push(elem.x);
+// });
+
+// console.log(result);
