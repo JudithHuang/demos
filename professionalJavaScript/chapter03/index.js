ed8282ec0cacd98b2adef24281591de1e557da6b
Judithhuang <judithhuang@augmentum.com.cn>
2016-10-27 08:36:56 +0800

    professionalJS: chapter 03

---
+++ professionalJavaScript/chapter03/index.js
@@ -0,0 +1,16 @@
+// 3.4
+// 3.4.6
+// eg.1
+// var str = "你好";
+// console.log(str.length);
+
+
+// 3.5.3
+// eg.1
+// var first = false;
+// var result = first && second;
+// console.log(result);
+
+// console.log('2' - '3');
+
+// 3.5.7
